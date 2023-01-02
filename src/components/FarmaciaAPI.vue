f744a76b386b364dcf727710a439eabbdcf066a8
javier <jgomez@edutecno.com>
2023-01-02 15:17:03 -0300

    first commit

---
+++ src/components/FarmaciaAPI.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>hola farmacia</h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'farmaciaAPI',
+  data () {
+    return {
+      info: null
+    }
+  },
+  async mounted () {
+    axios
+      .get('https://farmanet.minsal.cl/index.php/ws/getLocalesTurnos')
+      .then(response => (this.info = response))
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <FarmaciaAPI></FarmaciaAPI>
   </div>
 </template>
+
+<script>
+
+import { defineComponent } from 'vue'
+import FarmaciaAPI from '@/components/FarmaciaAPI.vue'
+
+export default defineComponent({
+  components: { FarmaciaAPI }
+})
+</script>
